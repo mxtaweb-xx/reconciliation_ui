30976aa4497a731c7a9ce7891d1100b2f9f1cc17
Peter Burns <peter@metaweb.com>
2009-06-08 20:35:16 -0700

    De-emphasize reconciliations where the name is exactly the same

--- review.js
+++ review.js
@@ -7,12 +7,25 @@ function renderReviews() {
         if (entity.id === null || entity.id === undefined || entity.id === "")
             return
         if (entity.id === "None")
-            template = $("#reviewNewTemplate").clone();
+            template = $(".templates .reviewNewTemplate").clone();
         else
-            template = $("#reviewReconciledTemplate").clone();
+            template = $(".templates .reviewReconciledTemplate").clone();
         
         $(".candidateName",template).html("<a class='internalLink' href='#" + entity['/rec_ui/id'] + "'>" + textValue(entity) + "</a>");
-        $(".freebaseName", template).html("<a href='"+freebase_url+"/view/"+entity.id+"'>"+entity['/rec_ui/freebase_name'] || entity.id+"</a>");
+        var freebaseName = null;
+        if (entity['/rec_ui/freebase_name']){
+            $.each(entity['/rec_ui/freebase_name'],function(idx,name){
+                if (name.toLowerCase() === textValue(entity).toLowerCase())
+                    freebaseName = name;
+            })
+            freebaseName = freebaseName || entity['/rec_ui/freebase_name'][0];
+        }
+        freebaseName = freebaseName || entity.id;
+        
+        $(".freebaseName", template).html("<a href='"+freebase_url+"/view/"+entity.id+"'>"+freebaseName+"</a>");
+        if (textValue(entity).toLowerCase() === freebaseName.toLowerCase())
+            $(".freebaseName", template).addClass("identicalMatch");
+            
         $(".internalLink", template).click(function(val) {
             $.historyLoad(entity["/rec_ui/id"]);
             $("#tabs > ul").tabs("select",0);})
